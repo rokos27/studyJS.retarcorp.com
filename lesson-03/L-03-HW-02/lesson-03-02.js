41556106d1f23fae5bdbc2143dc16f7463ff1686
rokos27 <rokos.job@gmail.com>
2019-07-04 18:06:49 +0300

    lesson 3 tasks 1, 2. fixed issues.

--- lesson-03/L-03-HW-02/lesson-03-02.js
+++ lesson-03/L-03-HW-02/lesson-03-02.js
@@ -9,62 +9,72 @@
 // и заставить каждого человека представиться.
 
 var namesArr = [
-    'Alexey'
-    ,'Andrei'
-    ,'Boris'
-    ,'Vadim'
-    ,'Victor'
-    ,'Georgiy'
-    ,'Denis'
-    ,'Ivan'
-    ,'Iosif'
-    ,'Moisey'
-    , 'Alisa'
-    , 'Vera'
-    , 'Daria'
-    , 'Elena'
-    , 'Zinaida'
-    , 'Inna'
-    , 'Ksenia'
-    , 'Maria'
-    , 'Nina'
-    , 'Svetlana'
+    'Alexey',
+    'Andrei',
+    'Boris',
+    'Vadim',
+    'Victor',
+    'Georgiy',
+    'Denis',
+    'Ivan',
+    'Iosif',
+    'Moisey',
+    'Alisa',
+    'Vera',
+    'Daria',
+    'Elena',
+    'Zinaida',
+    'Inna',
+    'Ksenia',
+    'Maria',
+    'Nina',
+    'Svetlana'
 ];
 var minAge = 3;     // уже умеем говорить
 var maxAge = 90;    // еще можем говорить
 var n = 10;        // кол-во объектов типа человек
-var sayNameAge = function (name, age) {
+var sayNameAge = function () {
     console.log(
-        'My name is ' + name
-        + '. I am ' + age + '.'
+        'My name is ' + this.name
+        + '. I am ' + this.age + '.'
     );
 };
-var sayAgeName = function (name, age) {
+var sayAgeName = function () {
     console.log(
-        'I am ' + age + '. My name is ' + name + '.'
+        'I am ' + this.age + '. My name is ' + this.name + '.'
     );
 
 };
 
+// вариант 1
 var people = [];
-
 for (var i = 0; i < n; i++) {
-    (function(x){
-        people[x] = {
-            name: namesArr[Math.floor(namesArr.length * Math.random())]
-            ,age: Math.floor(minAge + (maxAge - minAge) * Math.random())
-            ,myFunc: function() {
-                console.log(
-                    'My name is ' + this.name
-                    + '. I am ' + this.age + '.'
-                );
-            }
-        };
-    })(i);
+    people.push({
+        name: namesArr[Math.floor(namesArr.length * Math.random())],
+        age: Math.floor(minAge + (maxAge - minAge) * Math.random()),
+        myFunc: (Math.floor(Math.random() * 2) === 0) ? sayNameAge : sayAgeName
+    });
 }
 
 console.log(people);
 
 people.forEach(function (elem) {
+    elem.myFunc();
+});
+
+// вариант 2
+var people2 = new Array(n)
+    .fill(0)
+    .map( function() {
+        return {
+            name: namesArr[Math.floor(namesArr.length * Math.random())],
+            age: Math.floor(minAge + (maxAge - minAge) * Math.random()),
+            myFunc: (Math.floor(Math.random() * 2) === 0) ? sayNameAge : sayAgeName
+        };
+    });
+
+console.log(people2);
+
+people2.forEach(function (elem) {
     elem.myFunc();
 });
