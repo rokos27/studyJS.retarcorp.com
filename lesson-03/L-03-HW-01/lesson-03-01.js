dd71cff129fdf2129c34a4ba2c03eb8411630cb0
rokos27 <rokos.job@gmail.com>
2019-07-05 10:13:45 +0300

    lesson 3 tasks 1, 2. work-branch = master

--- lesson-03/L-03-HW-01/lesson-03-01.js
+++ lesson-03/L-03-HW-01/lesson-03-01.js
@@ -5,24 +5,19 @@
 
 var A = 400;        // медиана диапазона: s
 var B = 50;         // отклонение диапазона: ± p%
-var n = 1000;       // кол-во элементов исходного массива
+var n = 100;       // кол-во элементов исходного массива
 
 
 var minA = Math.floor(A - A * B / 100);
 var maxA = Math.floor(A + A * B / 100);
 
-var arr = new Array(n);
-arr.fill(0);
-
-for (var i in arr) {
-    arr[i] = Math.floor(minA + (maxA - minA) * Math.random());
-}
-
-arr.sort(function (a, b) {
-    return  a - b;
-})
+var arr = new Array(n)
+    .fill(0)
+    .map(function(){
+        return Math.floor(minA + (maxA - minA) * Math.random());
+    })
     .sort(function (a, b) {
-    return  b % 10 - a % 10;
-});
+        return  b % 10 - a % 10;
+    });
 
 console.log(arr);
--- lesson-03/L-03-HW-02/lesson-03-02.js
+++ lesson-03/L-03-HW-02/lesson-03-02.js
@@ -9,26 +9,26 @@
 // и заставить каждого человека представиться.
 
 var namesArr = [
-    'Alexey'
-    ,'Andrei'
-    ,'Boris'
-    ,'Vadim'
-    ,'Victor'
-    ,'Georgiy'
-    ,'Denis'
-    ,'Ivan'
-    ,'Iosif'
-    ,'Moisey'
-    , 'Alisa'
-    , 'Vera'
-    , 'Daria'
-    , 'Elena'
-    , 'Zinaida'
-    , 'Inna'
-    , 'Ksenia'
-    , 'Maria'
-    , 'Nina'
-    , 'Svetlana'
+    'Alexey',
+    'Andrei',
+    'Boris',
+    'Vadim',
+    'Victor',
+    'Georgiy',
+    'Denis',
+    'Ivan',
+    'Iosif',
+    'Moisey',
+    'Alisa',
+    'Vera',
+    'Daria',
+    'Elena',
+    'Zinaida',
+    'Inna',
+    'Ksenia',
+    'Maria',
+    'Nina',
+    'Svetlana'
 ];
 var minAge = 3;     // уже умеем говорить
 var maxAge = 90;    // еще можем говорить
@@ -43,22 +43,40 @@ var sayAgeName = function () {
     console.log(
         'I am ' + this.age + '. My name is ' + this.name + '.'
     );
+
 };
 
+// вариант 1
 var people = [];
-
 for (var i = 0; i < n; i++) {
-    (function(x){
-        people[x] = {
-            name: namesArr[Math.floor(namesArr.length * Math.random())]
-            ,age: Math.floor(minAge + (maxAge - minAge) * Math.random())
-            ,myFunc: (Math.floor(Math.random() * 2) === 0) ? sayNameAge : sayAgeName
-        };
-    })(i);
+    people.push({
+        name: namesArr[Math.floor(namesArr.length * Math.random())],
+        age: Math.floor(minAge + (maxAge - minAge) * Math.random()),
+        myFunc: Math.random() < 0.5 ? sayNameAge : sayAgeName
+        // myFunc: (Math.floor(Math.random() * 2) === 0) ? sayNameAge : sayAgeName
+    });
 }
 
 console.log(people);
 
 people.forEach(function (elem) {
+    elem.myFunc();
+});
+
+// вариант 2
+var people2 = new Array(n)
+    .fill(0)
+    .map( function() {
+        return {
+            name: namesArr[Math.floor(namesArr.length * Math.random())],
+            age: Math.floor(minAge + (maxAge - minAge) * Math.random()),
+            myFunc: Math.random() < 0.5 ? sayNameAge : sayAgeName
+            // myFunc: (Math.floor(Math.random() * 2) === 0) ? sayNameAge : sayAgeName
+        };
+    });
+
+console.log(people2);
+
+people2.forEach(function (elem) {
     elem.myFunc();
 });
